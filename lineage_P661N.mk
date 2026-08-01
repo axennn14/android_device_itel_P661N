bcf466b07f98f661971ed737898a0d44fedcb80d
axennn14 <basadregian18@gmail.com>
2026-08-01 10:21:43 +0800

    Update comment for Lineage inheritance in makefile

--- lineage_P661N.mk
+++ lineage_P661N.mk
@@ -7,7 +7,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/itel/P661N/device.mk)
-# Inherit some common AviumUI stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_P661N
